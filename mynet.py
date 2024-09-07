01701500a9c966780ba38b27c3f0a11bdc4fb79d
clay <uuz3905013631@outlook.com>
2024-09-07 12:30:22 +0800

    .

--- mynet.py
+++ mynet.py
@@ -1,14 +1,16 @@
-import torch
 import torch.nn as nn
+
 class mynet(nn.Module):
     def __init__(self,feature_extractor,class_num):
         super(mynet, self).__init__()
         self.feature_extractor = feature_extractor
         self.fc1 = nn.Linear(512, class_num)
+        
     def forward(self, x):
         x = self.feature_extractor(x)
         x = self.fc1(x)
         return x
+    
     def incremental_learning(self, class_num):
         in_features = self.fc1.in_features
         out_features = self.fc1.out_features
